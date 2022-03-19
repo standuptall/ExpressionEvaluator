49b54bd69e80169d070fe2b97dfb3bab5c9695ee
Alberto <a.zichittella@kfi.it>
2022-03-19 19:50:14 +0100

    ops sorry

---
+++ ExpressionEvaluator/Variabile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionEvaluator
+{
+    public class Variabile
+    {
+        public Variabile(string name,double value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+        public string Name { get; set; }
+        public double Value { get; set; }
+    }
+}
